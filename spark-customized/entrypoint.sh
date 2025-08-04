f92f3013bc9729672af0f79e32779393a29c9514
Aakcht <aakcht@gmail.com>
2025-08-04 13:34:59 +0300

    Update spark version

--- spark-customized/entrypoint.sh
+++ spark-customized/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# The file is mostly similar to https://github.com/apache/spark-docker/blob/master/3.5.5/scala2.12-java17-ubuntu/entrypoint.sh
+# The file is mostly similar to https://github.com/apache/spark-docker/blob/master/4.0.0/scala2.13-java21-ubuntu/entrypoint.sh
 # except logic for modifying certificates is added and history-server entrypoint support is added
 #
 # Prevent any errors from being silently ignored
