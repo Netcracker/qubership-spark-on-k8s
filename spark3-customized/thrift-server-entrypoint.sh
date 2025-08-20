4b29bce0812f2de33b8160c47b536cfb355ce2f3
Mohanakrishna Sadhu <mohanakrishna.sadhu@netcracker.com>
2025-08-20 10:20:57 +0530

    customized spark3 images

---
+++ spark3-customized/thrift-server-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Spark image logic used for user name from https://github.com/apache/spark/blob/master/resource-managers/kubernetes/docker/src/main/dockerfiles/spark/entrypoint.sh
+
+# echo commands to the terminal output
+set -ex
+
+# Check whether there is a passwd entry for the container UID
+myuid=$(id -u)
+mygid=$(id -g)
+# turn off -e for getent because it will return error code in anonymous uid case
+set +e
+uidentry=$(getent passwd $myuid)
+set -e
+
+# If there is no passwd entry for the container UID, attempt to create one
+if [ -z "$uidentry" ] ; then
+    if [ -w /etc/passwd ] ; then
+        echo "$myuid:x:$myuid:$mygid:${HIVE_USER_NAME:-anonymous uid}:$SPARK_HOME:/bin/false" >> /etc/passwd
+    else
+        echo "Container ENTRYPOINT failed to add passwd entry for anonymous UID"
+    fi
+fi
+
+/opt/spark/bin/spark-submit --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 --name Thrift JDBC/ODBC Server --conf spark.driver.bindAddress="${THRIFT_POD_IP}"
