adcd249ae62cb1b476f338aa829b2d4e502ea823
Mohanakrishna Sadhu <mohanakrishna.sadhu@netcracker.com>
2025-09-30 15:03:33 +0530

    lint fix

--- spark-operator-image/operator-entrypoint.sh
+++ spark-operator-image/operator-entrypoint.sh
@@ -3,21 +3,21 @@ set -ex
 
 myuid="$(id -u)"
 if ! getent passwd "$myuid" >/dev/null 2>&1; then
-    for wrapper in {/usr,}/lib{/*,}/libnss_wrapper.so; do
+    for wrapper in /usr/lib/libnss_wrapper.so /usr/libnss_wrapper.so /lib/libnss_wrapper.so /libnss_wrapper.so; do
       if [ -s "$wrapper" ]; then
         NSS_WRAPPER_PASSWD="$(mktemp)"
         NSS_WRAPPER_GROUP="$(mktemp)"
         export LD_PRELOAD="$wrapper" NSS_WRAPPER_PASSWD NSS_WRAPPER_GROUP
         mygid="$(id -g)"
-        printf 'spark:x:%s:%s:${SPARK_USER_NAME:-anonymous uid}:%s:/bin/false\n' \
+        printf "spark:x:%s:%s:${SPARK_USER_NAME:-anonymous uid}:%s:/bin/false\n" \
           "$myuid" "$mygid" "$SPARK_HOME" > "$NSS_WRAPPER_PASSWD"
-        printf 'spark:x:%s:\n' "$mygid" > "$NSS_WRAPPER_GROUP"
+        printf "spark:x:%s:\n" "$mygid" > "$NSS_WRAPPER_GROUP"
         break
       fi
     done
 fi
 
-if [ -n "${S3_CERTS_DIR}" ] && [ "$(ls -A ${S3_CERTS_DIR})" ]; then
+if [ -n "${S3_CERTS_DIR}" ] && [ "$(ls -A "${S3_CERTS_DIR}")" ]; then
 
     : "${JAVA_WRITABLE_KEYSTORE:?Environment variable JAVA_WRITABLE_KEYSTORE must be set}"
 
@@ -28,7 +28,7 @@ if [ -n "${S3_CERTS_DIR}" ] && [ "$(ls -A ${S3_CERTS_DIR})" ]; then
 
     for filename in "${S3_CERTS_DIR}"/*; do
         echo "Importing $filename into Java truststore..."
-        ${JAVA_HOME}/bin/keytool -importcert \
+        "${JAVA_HOME}/bin/keytool" -importcert \
           -trustcacerts \
           -keystore "$JAVA_WRITABLE_KEYSTORE" \
           -storepass changeit \
@@ -39,6 +39,5 @@ if [ -n "${S3_CERTS_DIR}" ] && [ "$(ls -A ${S3_CERTS_DIR})" ]; then
 
     export JAVA_TOOL_OPTIONS="-Djavax.net.ssl.trustStore=$JAVA_WRITABLE_KEYSTORE -Djavax.net.ssl.trustStorePassword=changeit"
 fi
-
 
 exec /usr/bin/tini -s -- /usr/bin/spark-operator "$@"
