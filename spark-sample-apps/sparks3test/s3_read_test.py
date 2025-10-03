734717255f9aca90bfdc75941a6766759692e69c
Mohanakrishna Sadhu <mohanakrishna.sadhu@netcracker.com>
2025-10-03 17:42:21 +0530

    final changes

--- spark-sample-apps/sparks3test/s3_read_test.py
+++ spark-sample-apps/sparks3test/s3_read_test.py
@@ -1,11 +1,13 @@
 from pyspark.sql import SparkSession
 import sys
+import os
 
 # Create Spark session
-spark = SparkSession.builder.appName("S3 JSON Test").getOrCreate()
+spark = SparkSession.builder.appName("S3 connection Test").getOrCreate()
+s3_file = os.getenv("S3_JSON_FILE")
 
 # Path to JSON file (passed via pyFiles or directly in code)
-json_file = "s3a://sparktest/test.JSON"
+json_file = s3_file
 
 # Read JSON
 df = spark.read.json(json_file)
