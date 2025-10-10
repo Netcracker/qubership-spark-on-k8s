167f0833bec4c94ee16b5280ae89980e887bb360
Mohanakrishna Sadhu <mohanakrishna.sadhu@netcracker.com>
2025-10-10 10:35:43 +0530

    linters fix

--- spark-operator-image/operator-entrypoint.sh
+++ spark-operator-image/operator-entrypoint.sh
@@ -18,7 +18,6 @@ if ! getent passwd "$myuid" >/dev/null 2>&1; then
 fi
 
 if [ -n "${S3_CERTS_DIR}" ] && [ "$(ls -A "${S3_CERTS_DIR}")" ]; then
-
     : "${JAVA_WRITABLE_KEYSTORE:?Environment variable JAVA_WRITABLE_KEYSTORE must be set}"
 
     if [ ! -f "$JAVA_WRITABLE_KEYSTORE" ]; then
@@ -40,13 +39,19 @@ if [ -n "${S3_CERTS_DIR}" ] && [ "$(ls -A "${S3_CERTS_DIR}")" ]; then
     export JAVA_TOOL_OPTIONS="-Djavax.net.ssl.trustStore=$JAVA_WRITABLE_KEYSTORE -Djavax.net.ssl.trustStorePassword=changeit"
 fi
 
-if [[ -f $TLS_KEY_PATH && -f $TLS_CERT_PATH ]]; then
-  if [ ! -d "${TLS_KEYSTORE_DIR}" ]; then
+
+if [ -f "$TLS_KEY_PATH" ] && [ -f "$TLS_CERT_PATH" ]; then
+  if [ ! -d "$TLS_KEYSTORE_DIR" ]; then
     echo "Creating keystore directory"
-    mkdir -p ${TLS_KEYSTORE_DIR}
+    mkdir -p "$TLS_KEYSTORE_DIR"
   fi
+
   echo "Adding to keystore"
-  openssl pkcs12 -export -in ${TLS_CERT_PATH} -inkey ${TLS_KEY_PATH} -out ${TLS_KEYSTORE_DIR}/keystore.p12 -passout pass:${TLS_KEYSTORE_PASSWORD}
+  openssl pkcs12 -export \
+    -in "$TLS_CERT_PATH" \
+    -inkey "$TLS_KEY_PATH" \
+    -out "$TLS_KEYSTORE_DIR/keystore.p12" \
+    -passout pass:"$TLS_KEYSTORE_PASSWORD"
 fi
 
 exec /usr/bin/tini -s -- /usr/bin/spark-operator "$@"
