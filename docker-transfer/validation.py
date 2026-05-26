fdde9b3690e025801a6f297987e057e841f22c83
mosa0519 <mohanakrishna.sadhu@netcracker.com>
2026-05-26 13:49:01 +0530

    fix: Enhance validation logic for default values in validation.py

--- docker-transfer/validation.py
+++ docker-transfer/validation.py
@@ -11,7 +11,8 @@ def validate_defaults(values, default_values_schema, param_path=""):
                 schema_prop = default_values_schema["properties"][key]
 
                 if "default" in schema_prop and not isinstance(value, (dict, list)):
-                    if schema_prop["default"] == "see the values.yaml file":
+                    default_val = schema_prop.get("default", "")
+                    if ( isinstance(default_val, str) and default_val.startswith("see ") and default_val.endswith(" values.yaml file") ):
                         print(
                             f"Skipping validation for this parameter: {param_path}{key}."
                         )
